647b40e1997e65d7a900180021d421e3e8a7d118
elfmaster <ryan@bitlackeys.org>
2025-03-10 16:17:46 -0700

    began making good headway on gASLR module

--- modules/x86_64_modules/aslr/aslr.c
+++ modules/x86_64_modules/aslr/aslr.c
@@ -23,7 +23,11 @@ typedef struct reloc_entry {
 #define ASLR_FUNC_F_ENTRYPOINT	(1 << 0) // is pointed to by ehdr->e_entry
 
 typedef struct func_entry {
-	uint64_t base_vaddr;
+	uint64_t base_vaddr;	// on disk
+	uint64_t runtime_vaddr; // after initial ASLR at runtime
+	uint64_t new_base_vaddr; // at runtime after Granular ASLR (New location)
+	uint8_t *o_mem;		// memory of old location
+	uint8_t *n_mem;		// memory mapping of new location
 	struct elf_symbol symbol;
 	struct elf_section section;
 	size_t func_len;
@@ -40,6 +44,7 @@ typedef struct aslr_ctx {
 	uint64_t base_vaddr;
 } aslr_ctx_t;
 
+struct elf_section text_section, got_section;
 
 bool
 build_func_list(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
@@ -62,7 +67,12 @@ build_func_list(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
 		fprintf(stderr, "Failed to get section .text\n");
 		return false;
 	}
+	text_section = text;
 
+	if (elf_section_by_name(&ctx->elfobj, ".got", &got_section) == false ) {
+		fprintf(stderr, "Failed to get section .got\n");
+		return false;
+	}
 	printf("Iterating over functions\n");
 	elf_symtab_iterator_init(&ctx->elfobj, &sym_iter);
 	while (elf_symtab_iterator_next(&sym_iter, &symbol) == ELF_ITER_OK) {
@@ -86,8 +96,11 @@ build_func_list(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
 			/*
 			 * This function lives in the .text
 			 */
-			if (symbol.value == elf_entry_point(&ctx->elfobj))
+			if (symbol.value == elf_entry_point(&ctx->elfobj)) {
+				printf("Found function %s with entrypoint\n",
+				    symbol.name);
 				fe->flags |= ASLR_FUNC_F_ENTRYPOINT;
+			}
 			fe->symbol = symbol;
 			fe->section = text;
 			fe->base_vaddr = symbol.value;
@@ -97,12 +110,12 @@ build_func_list(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
 
 			/*
 			 * The .rela.text section in an ELF executable will
-			 * contain r_offset values that are absolute vs.
-			 * relative to the beginning of the .text section
-			 * as is the case with ET_REL objects. This makes
-			 * sense since the absolute addresses didn't exist
-			 * until the ET_REL objects were linked into a final
-			 * executable, and thus the relocation tables get
+			 * contain r_offset values that are absolute values vs.
+			 * values that are relative to the beginning of the
+			 * .text section as is the case with ET_REL objects.
+			 * This makes sense since the absolute addresses didn't
+			 * exist until the ET_REL objects were linked into a
+			 * final executable, and thus the relocation tables get
 			 * updated with the absolute r_offset's.
 			 *
 			 * NOTE: See -z --emit-relocs
@@ -132,6 +145,18 @@ build_func_list(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
 			}
 			printf("Inserting function %s\n", fe->symbol.name);
 			fe->old_entry_number = fn_count;
+			fe->runtime_vaddr = fe->base_vaddr + ctx->ulexec.base_vaddr;
+			/*
+			 * Create new memory mapping to move function into.
+			 */
+			fe->n_mem = mmap(NULL, fe->func_len, PROT_READ|PROT_WRITE|PROT_EXEC,
+			    MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+			if (fe->n_mem == MAP_FAILED) {
+				perror("mmap");
+				return false;
+			}
+			fe->new_base_vaddr = (uint64_t)fe->n_mem;
+			fe->o_mem = (uint8_t *)fe->runtime_vaddr;
 			TAILQ_INSERT_TAIL(&aslr->orig_func_list, fe, _linkage);
 			*fn_count++;
 		}
@@ -140,30 +165,83 @@ build_func_list(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
 }
 
 bool
-reorder_func_list(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
+relocate_function(struct shiva_ctx *ctx, struct aslr_ctx *aslr, struct func_entry *fe)
+{
+	struct reloc_entry *rel_entry;
+	uint64_t plt_addr; // address of a specific PLT entry
+	uint64_t gotoff; // offset of beginning of GOT within the executable
+
+	TAILQ_FOREACH(rel_entry, &fe->reloc_list, _linkage) {
+		uint8_t *r_ptr = fe->n_mem + rel_entry.rel.offset;
+		uint64_t rel_addr = (uint64_t)r_ptr;
+
+		switch(rel_entry.rel.type) {
+		case R_X86_64_GOTPCREL:
+			break;
+		case R_X86_64_REX_GOTPCRELX:
+			break;
+		case R_X86_64_PLT32:
+			res = elf_plt_by_name(&ctx->elfobj, fe->symbol.name,
+			    &plt);
+			if (res == false) {
+				fprintf(stderr, "elf_plt_by_name() failed on %s\n",
+				    fe->symbol.name);
+				return false;
+			}
+			plt_addr = plt.addr + ctx->ulexec.base_vaddr;
+			rel_val = plt_addr + rel_entry->rel.addend - rel_addr;
+			*(uint32_t *)r_ptr = rel_val;
+			break;
+		case R_X86_64_PC32:
+			if (rel_entry->rel.symname[0] == '.') {
+				res = elf_section_by_name(&ctx->elfobj, rel_entry->rel.symname,
+				    &shdr);
+				if (res == false) {
+					fprintf(stderr, "elf_section_by_name() on %s failed\n",
+					    rel_entry->rel.symname);
+					return false;
+				}
+				/*
+				 * TODO
+				 */
+			}
+			break;
+	}
+	return true;
+}
+
+bool
+move_function(struct shiva_ctx *ctx, struct aslr_ctx *aslr, struct func_entry *fe)
+{
+
+	size_t delta;
+	struct reloc_entry *rel_entry;
+
+	memcpy(fe->n_mem, (uint8_t *)fe->runtime_vaddr, fe->func_len);
+	delta = fe->base_vaddr - text_section.address;
+	TAILQ_FOREACH(rel_entry, &fe->reloc_list, _linkage) {
+		printf("Updating relocation offset from %#lx to %#lx\n", 
+		rel_entry->rel.offset, rel_entry->rel.offset - delta);
+		rel_entry->rel.offset -= delta;
+	}
+	return relocate_function(ctx, aslr, fe);
+}
+bool
+randomize_func_locations(struct shiva_ctx *ctx, struct aslr_ctx *aslr,
     size_t fn_count)
 {
 	struct func_entry *fe;
-	int order_map[4096 * sizeof(size_t)]; // = malloc(fn_count * (sizeof(size_t)));
-	int order, i;
-
-	printf("1\n");
-	for (i = 0; i < fn_count; i++) {
-		printf("Setting order_map[%d]\n", i);
-		order_map[i] = -1;
-	}
-	printf("2\n");
-	TAILQ_INIT(&aslr->aslr_func_list);
+	bool res;
 
 	TAILQ_FOREACH(fe, &aslr->orig_func_list, _linkage) {
-		order = rand() % fn_count;
-		printf("order = %d\n", order);
-		if (order_map[order] != -1)
+		if (fe->flags & ASLR_FUNC_F_ENTRYPOINT)
 			continue;
-		printf("order_map[order] is being set to %d\n", rand() % fn_count);
-		order_map[order] = rand() % fn_count;
+		res = move_function(ctx, aslr, fe);		
+		if (res == false) {
+			fprintf(stderr, "Failed to move function %s\n", fe->symbol.name);
+			return false;
+		}
 	}
-	free(order_map);
 	return true;
 }
 
@@ -178,11 +256,9 @@ shiva_init(struct shiva_ctx *ctx)
 		fprintf(stderr, "build_func_list() failed on .text\n");
 		return -1;
 	}
-
-	printf("Calling reorder_func_list\n");
-	if (reorder_func_list(ctx, &aslr, fn_count) == false) {
-		fprintf(stderr, "reorder_func_list() failed\n");
+	
+	if (randomize_func_locations(ctx, &aslr, &fn_count) == false) {
+		fprintf(stderr, "randomize_func_locations() failed\n");
 		return -1;
 	}
-	printf("Exiting\n");
 }
--- modules/x86_64_modules/aslr/test.c
+++ modules/x86_64_modules/aslr/test.c
@@ -3,5 +3,4 @@
 int main(void)
 {
 	printf("Testing 1, 2, 3\n");
-	sleep(5);
 }
