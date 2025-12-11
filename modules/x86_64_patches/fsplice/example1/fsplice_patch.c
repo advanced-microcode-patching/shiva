e356b18070f700591f8c83147de38a1b75823846
elfmaster <ryan@bitlackeys.org>
2025-12-11 13:46:58 -0800

    cleaned up fsplice/example1 patch code a bit more

--- modules/x86_64_patches/fsplice/example1/fsplice_patch.c
+++ modules/x86_64_patches/fsplice/example1/fsplice_patch.c
@@ -5,10 +5,6 @@
 
 SHIVA_T_SPLICE_FUNCTION(foo, 0x114c, 0x115a)
 {
-	/*
-	 * Attach x0 (arg1) to a use-able variable. (Which
-	 * will live in the .bss).
-	 */
 	SHIVA_T_PAIR_RDI(str);
 	fprintf(stdout, "(fprintf version): Printing arg: %s\n", str);
 	bar();
--- modules/x86_64_patches/fsplice/example1/fsplice_patch2.c
+++ modules/x86_64_patches/fsplice/example1/fsplice_patch2.c
@@ -5,10 +5,6 @@
 
 SHIVA_T_SPLICE_FUNCTION_REPLACE_SRCLINE(foo, 5)
 {
-	/*
-	 * Attach x0 (arg1) to a use-able variable. (Which
-	 * will live in the .bss).
-	 */
 	SHIVA_T_PAIR_RDI(str);
 	fprintf(stdout, "(fprintf version): Printing arg: %s\n", str);
 	bar();
