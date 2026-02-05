d49ca7081a6309104d131bb0b900259769fdf2f1
elfmaster <ryan@bitlackeys.org>
2026-02-04 17:33:52 -0800

    updated bss interpose patch

--- modules/x86_64_patches/bss_interposing/bss_patch.c
+++ modules/x86_64_patches/bss_interposing/bss_patch.c
@@ -5,13 +5,8 @@ int bss_var; // shiva will re-link the programs bss_var to this one
 
 int foo(void) // shiva will re-link the programs foo() function to this one
 {
-	char *p;
 	bss_var = 0x31337; // This will of course assign 0x31337 to the patches bss_var
 	printf("I'm the new foo() function!\n");
 	printf("The new value of bss_var is %x\n", bss_var);
-
-	p = strdup("I was born on the heap");
-	printf("p: %s\n", p);
-	bar();
 	return 0;
 }
