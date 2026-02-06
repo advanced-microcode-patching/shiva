5700a75cf33e673c9223f6a91b0f0c52f35988dd
elfmaster <ryan@bitlackeys.org>
2026-02-05 19:16:05 -0800

    removed nasa_patch2.c

---
+++ modules/x86_64_patches/pacman/pacman_patch1.c
@@ -0,0 +1,34 @@
+/*
+ * Patch code for Pacman to make enemies all scared.
+ * 
+ * How it works: It modifies fgState->idle callback ptr
+ * to &my_idle(). When my_idle() is invoked it resets
+ * two global variables: frighten and frightenTick.
+ *
+ * Look into libglut.so:glutIdleFunc() for more
+ * understanding.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "/opt/shiva/include/shiva_module.h"
+
+extern uint8_t fgState;
+extern bool frighten;
+extern int frightenTick;
+
+void my_idle(void)
+{
+	frighten = true;
+	frightenTick = 0;
+	_Z4idlev();
+}
+
+void glutIdleFunc(void)
+{
+	uint8_t *fptr = (uint8_t *)&fgState + 104;
+
+	*(uint64_t *)fptr = &my_idle;
+}
---
+++ modules/x86_64_patches/pacman/pacman_patch2.c
@@ -0,0 +1,7 @@
+
+/*
+ * Pacman patch to give 31337 lives! Simply sets
+ * the global variable: int lives, to 31337.
+ */
+
+int lives = 31337;
