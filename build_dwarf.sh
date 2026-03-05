333c01597fe359f82a5739ffeb777b45d4707152
elfmaster <ryan@bitlackeys.org>
2026-03-05 15:36:50 -0800

    added --disable-decompresion flag to avoid zlib compression in building libdwarf

--- build_dwarf.sh
+++ build_dwarf.sh
@@ -1,5 +1,5 @@
 wget -qO- https://www.prevanders.net/libdwarf-0.10.1.tar.xz | tar xJ && \
 cd libdwarf-0.10.1 && \
-CC=musl-gcc CFLAGS="-Os -static" ./configure --prefix=/usr/local/musl --enable-static --disable-shared --with-libdwarf-include-style=libdwarf-2 --disable-debuginfod && \
+CC=musl-gcc CFLAGS="-Os -static" ./configure --prefix=/usr/local/musl --disable-decompression --enable-static --disable-shared --with-libdwarf-include-style=libdwarf-2 --disable-debuginfod && \
 make -j$(nproc) && sudo make install && \
 echo "libdwarf.a is now at /usr/local/musl/lib/libdwarf.a"
